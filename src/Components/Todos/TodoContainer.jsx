bd079d1b940f8b58751a15c5d5d2054f5609e28f
NishanThapa10 <107379632+NishanThapa10@users.noreply.github.com>
2024-02-04 15:04:23 +0545

    filtering todos

--- src/Components/Todos/TodoContainer.jsx
+++ src/Components/Todos/TodoContainer.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useState } from 'react'
+import useAxiosCall from '../../hooks/useAxiosCall'
+import { ReloadContext } from '../../Pages/TodoPage'
 import TodoButton from '../TodoButton'
 import TodoList from './TodoList'
 
 const TodoContainer = () =>{
+
+  const [filteredData , setFilteredData] = useState([]);
+
+  const {reload} = useContext(ReloadContext);
+
+  const todo = useAxiosCall("todos",reload);
+
+
+  const filterAll = () =>{
+    setFilteredData(todo);
+  }
+
+  const filterDone = () =>{
+    const data = todo?.filter((item) => item.completed);
+    setFilteredData(data);
+  }
+
+  const filterTodo = () =>{
+    const data = todo?.filter((item) => !item.completed);
+    setFilteredData(data);
+  }
+  
+
   return(
     <div className='mt-8 flex flex-col gap-4 mb-[60px]'>
       <h1 className='text-center text-2xl font-medium'>To do List</h1>
       <div className='flex gap-2'>
-        <TodoButton onClick={()=>{}} text={"All"}/>
-        <TodoButton onClick={()=>{}} text={"Done"}/>
-        <TodoButton onClick={()=>{}} text={"Todo"}/>
+        <TodoButton onClick={filterAll} text={"All"}/>
+        <TodoButton onClick={filterDone} text={"Done"}/>
+        <TodoButton onClick={filterTodo} text={"Todo"}/>
 
       </div>
-      <TodoList />
+      <TodoList todo={todo} filteredData={filteredData} />
     </div>
   )
 }
--- src/Components/Todos/TodoList.jsx
+++ src/Components/Todos/TodoList.jsx
@@ -1,40 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React from 'react'
 import Todo from './Todo'
-import {Axios} from '../../../api/server.js'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { ReloadContext } from '../../Pages/TodoPage';
-import useAxiosCall from '../../hooks/useAxiosCall';
 
-const TodoList = () => {  
-
-  const {reload} = useContext(ReloadContext);
-                                                                    
-  const [data, setData] = useState([]);
-  // const [reload, setReload] = useState(false);
-
-  // useEffect(()=>{
-  //   getAlllTodos();
-  // },[reload]);
-
-  const todo = useAxiosCall("todos",reload);
-
-  // const getAlllTodos = async () => {
-  //   try{
-  //     const res = await Axios.get("todos");
-  //     setData(res.data.reverse());
-  //     setReload((prev)=>!prev);
-  //   } catch (error){
-  //     toast.error("error fetching todo.");
-  //     // console.log(error);
-  //   }
-  // };
-
-
+const TodoList = ({todo, filteredData}) => {  
 
   return (
     <div className="flex flex-col gap-3">
-      {todo?.map((item)=>{
+      {filteredData.length > 0? filteredData?.map((item)=>{
+        return <Todo item={item}/>
+      })
+      : todo?.map((item)=>{
         return <Todo  item={item}/>
       })}
       
